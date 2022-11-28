c63f056592d0c6ea068d099e8ac8d6b763a2dddd
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-11-28 20:32:59 +0100

    Working on drivers section

---
+++ src/utils/calculateAge.ts
@@ -0,0 +1,5 @@
+export const calculateAge = (birthday: string): number => {
+    const ageDifMs = Date.now() - new Date(birthday).getTime();
+    const ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  }
