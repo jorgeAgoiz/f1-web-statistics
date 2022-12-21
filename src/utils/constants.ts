b5b86cda9ff793c3d13effcc4cb5317abd9026a4
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-12-21 20:40:33 +0100

    More refactors and small changes in styles

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+interface Option {
+  href: string;
+  title: string;
+}
+
+export const menuOptions: Array<Option> = [
+  {
+    href: "/",
+    title: "Inicio",
+  },
+  {
+    href: "/drivers",
+    title: "Pilotos",
+  },
+  {
+    href: "/teams",
+    title: "Equipos",
+  },
+  {
+    href: "/circuits",
+    title: "Circuitos",
+  },
+  {
+    href: "/about",
+    title: "Acerca de",
+  },
+];
