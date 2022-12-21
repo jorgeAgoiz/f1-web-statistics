2b3916a11bc928e150ab2d41854fa6429bfb9216
jorgeAgoiz <jorgeagoiz@gmail.com>
2022-12-21 20:48:03 +0100

    add Astro adapter (Node) to use SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,13 @@ import { defineConfig } from 'astro/config';
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [image()],
-  output: 'server'
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
